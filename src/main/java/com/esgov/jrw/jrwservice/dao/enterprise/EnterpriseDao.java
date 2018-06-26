246614c28337b391ca7b95a25fbbbb7a62c30225
chenbingyi <2583150126@qq.com>
2018-06-26 15:16:53 +0800

    Signed-off-by: chenbingyi <2583150126@qq.com>

--- src/main/java/com/esgov/jrw/jrwservice/dao/enterprise/EnterpriseDao.java
+++ src/main/java/com/esgov/jrw/jrwservice/dao/enterprise/EnterpriseDao.java
@@ -3,4 +3,5 @@ package com.esgov.jrw.jrwservice.dao.enterprise;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.esgov.jrw.jrwservice.entity.enterprise.Enterprise;
 public interface EnterpriseDao extends BaseMapper<Enterprise> {
+    public void get();
 }
